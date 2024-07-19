b7a70f62efef43abbbd8b6620cc1551c14784785
Jack Hamilton <jack.hamilton@grindr.com>
2024-07-19 13:39:56 -0500

    func†ional on mac

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
     # Home Manager needs a bit of information about you and the paths it should
     # manage.
     home.username = "jackhamilton";
-    home.homeDirectory = "/home/jackhamilton";
+    home.homeDirectory = "/Users/jackhamilton";
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release

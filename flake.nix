a7f2ba36240dfccc96841ffe4c1d6718f957145c
Jack Hamilton <jack.hamilton@grindr.com>
2024-07-19 12:44:24 -0500

    23.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
     description = "Home manager configuration";
 
     inputs = {
-        nixpkgs.url = "nixpkgs-24.05";
+        nixpkgs.url = "nixpkgs-23.11";
 
         home-manager = {
-            url = "github:nix-community/home-manager/release-24.05";
+            url = "github:nix-community/home-manager/release-23.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };

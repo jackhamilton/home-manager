3c86e14e5fd191b84e640bbb577b901be6760236
Jack Hamilton <jack.hamilton@grindr.com>
2024-07-19 12:40:11 -0500

    nixfiles

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+    description = "Home manager configuration";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs-24.05";
+
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { nixpkgs, home-manager, ... }:
+        let
+          lib = nixpkgs.lib;
+          system = "x86_64-darwin";
+          pkgs = import nixpkgs { inherit system; };
+        in {
+            homeConfigurations = {
+                jack = home-manager.lib.homeManagerConfiguration {
+                        inherit pkgs;
+                        modules = [ ./.config/home-manager/home.nix ];
+                };
+            };
+        };
+}

f10244e5c66123e2b890ac8b7de079f389b81d8f
Jack Hamilton <jackham800@gmail.com>
2025-09-30 19:40:46 -0500

    update home.nix

--- home.nix
+++ home.nix
@@ -1,25 +1,58 @@
 { config, pkgs, lib, username, homeDirectory, ... }:
 
-let
-  isDarwin = pkgs.stdenv.hostPlatform.isDarwin or pkgs.stdenv.isDarwin;
+let isDarwin = pkgs.stdenv.hostPlatform.isDarwin or pkgs.stdenv.isDarwin;
 in {
-  home.username      = username;
+  home.username = username;
   home.homeDirectory = homeDirectory;
-  home.stateVersion  = "24.05";
+  home.stateVersion = "24.05";
 
   home.packages = with pkgs;
     [
-      lsd bat curlie zoxide bitwarden-cli chezmoi cloc fzf difftastic cowsay
-      fd github-cli imagemagick pipx ripgrep sccache wget sesh grex hyperfine
-      pastel sd watchexec xcp go lazygit neovim htop tmux
-    ]
-    ++ lib.optionals isDarwin [ xcbeautify ]
+      lsd
+      bat
+      curlie
+      zoxide
+      bitwarden-cli
+      chezmoi
+      cloc
+      fzf
+      difftastic
+      cowsay
+      fd
+      github-cli
+      imagemagick
+      pipx
+      ripgrep
+      sccache
+      wget
+      sesh
+      grex
+      hyperfine
+      pastel
+      sd
+      watchexec
+      xcp
+      go
+      lazygit
+      neovim
+      htop
+      tmux
+      nixfmt
+    ] ++ lib.optionals isDarwin [ xcbeautify ]
     ++ lib.optionals (!isDarwin) [ wezterm handbrake krita inkscape ];
 
   programs.git = {
     enable = true;
     userName = "jackhamilton";
     userEmail = "jackham800@gmail.com";
+    extraConfig = ''
+      [core]
+          attributesFile = ${config.home.homeDirectory}/.config/git/attributes
+
+      [merge "mergiraf"]
+          name = mergiraf
+          driver = mergiraf merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L
+    '';
   };
 
   programs.direnv = {
@@ -30,6 +63,78 @@ in {
 
   programs.starship.enable = true;
   programs.home-manager.enable = true;
+
+  home.file.".config/git/attributes".text = ''
+    *.java merge=mergiraf
+    *.properties merge=mergiraf
+    *.kt merge=mergiraf
+    *.rs merge=mergiraf
+    *.go merge=mergiraf
+    go.mod merge=mergiraf
+    go.sum merge=mergiraf
+    *.ini merge=mergiraf
+    *.js merge=mergiraf
+    *.jsx merge=mergiraf
+    *.mjs merge=mergiraf
+    *.json merge=mergiraf
+    *.yml merge=mergiraf
+    *.yaml merge=mergiraf
+    pyproject.toml merge=mergiraf
+    *.toml merge=mergiraf
+    *.html merge=mergiraf
+    *.htm merge=mergiraf
+    *.xhtml merge=mergiraf
+    *.xml merge=mergiraf
+    *.c merge=mergiraf
+    *.h merge=mergiraf
+    *.cc merge=mergiraf
+    *.hh merge=mergiraf
+    *.cpp merge=mergiraf
+    *.hpp merge=mergiraf
+    *.cxx merge=mergiraf
+    *.hxx merge=mergiraf
+    *.c++ merge=mergiraf
+    *.h++ merge=mergiraf
+    *.mpp merge=mergiraf
+    *.cppm merge=mergiraf
+    *.ixx merge=mergiraf
+    *.tcc merge=mergiraf
+    *.cs merge=mergiraf
+    *.dart merge=mergiraf
+    *.dts merge=mergiraf
+    *.scala merge=mergiraf
+    *.sbt merge=mergiraf
+    *.ts merge=mergiraf
+    *.tsx merge=mergiraf
+    *.py merge=mergiraf
+    *.php merge=mergiraf
+    *.phtml merge=mergiraf
+    *.sol merge=mergiraf
+    *.lua merge=mergiraf
+    *.rb merge=mergiraf
+    *.ex merge=mergiraf
+    *.exs merge=mergiraf
+    *.nix merge=mergiraf
+    *.sv merge=mergiraf
+    *.svh merge=mergiraf
+    *.md merge=mergiraf
+    *.hcl merge=mergiraf
+    *.tf merge=mergiraf
+    *.tfvars merge=mergiraf
+    *.ml merge=mergiraf
+    *.mli merge=mergiraf
+    *.hs merge=mergiraf
+    *.mk merge=mergiraf
+    Makefile merge=mergiraf
+    GNUmakefile merge=mergiraf
+    *.bzl merge=mergiraf
+    *.bazel merge=mergiraf
+    BUILD merge=mergiraf
+    WORKSPACE merge=mergiraf
+    *.cmake merge=mergiraf
+    *.swift merge=mergiraf
+    CMakeLists.txt merge=mergiraf
+  '';
 
   home.sessionVariables = {
     EDITOR = "nvim";

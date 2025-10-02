b03038c771403a0ae6816e4d3544020dfe0655a2
Jack Hamilton <jackham800@gmail.com>
2025-10-02 14:22:14 -0500

    organization

--- home.nix
+++ home.nix
@@ -9,43 +9,57 @@ in {
     nixpkgs.config.allowUnfree = true;
 
     home.packages = with pkgs;
-        (with pkgs; [
-            lsd
-            bat
-            curlie
-            zoxide
-            cloc
-            fzf
-            difftastic
-            cowsay
-            fd
-            github-cli
-            imagemagick
-            pipx
-            ripgrep
-            sccache
-            wget
-            sesh
-            grex
-            hyperfine
-            pastel
-            sd
-            watchexec
-            xcp
-            go
-            lazygit
-            htop
-            tmux
-            nixfmt-rfc-style
-            rbenv
-	        neofetch
-        ]
-            ++ lib.optionals isDarwin [ xcbeautify ]
-            ++ lib.optionals (!isDarwin) [ wezterm handbrake krita inkscape ])
-        ++ (with pkgs-unstable; [
-            mergiraf
-            neovim
-        ]);
+    (with pkgs; [
+     lsd
+     bat
+     curlie
+     zoxide
+     cloc
+     fzf
+     difftastic
+     cowsay
+     fd
+     github-cli
+     imagemagick
+     pipx
+     ripgrep
+     sccache
+     wget
+     sesh
+     grex
+     hyperfine
+     pastel
+     sd
+     watchexec
+     xcp
+     go
+     lazygit
+     htop
+     tmux
+     nixfmt-rfc-style
+	 neofetch
+     ]
+     ++ (with pkgs-unstable; [
+             # MARK: Unstable
+             mergiraf
+             neovim
+     ])
+     ++ lib.optionals isDarwin [
+     # MARK: Macos only
+     xcbeautify
+     ]
+     ++ lib.optionals (!isDarwin) [
+     # MARK: Linux only
+     wezterm
+     ]);
+
+
+    services.cron = {
+        enable = true;
+        systemCronJobs = [
+            "0 0 * * *      root    nix-channel --update"
+        ];
+    };
 
     programs.git = {
         enable = true;
@@ -57,10 +71,11 @@ in {
                 driver = "mergiraf merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
             };
             core.attributesfile = "${config.home.homeDirectory}/.config/git/attributes";
-            diff.tool = difft;
-            credential.https://github.com.helper = "!${pkgs.github-cli}/bin/gh auth git-credential";
-            credential.https://gist.github.com.helper = "!${pkgs.github-cli}/bin/gh auth git-credential";
+            diff.tool = "difft";
+            credential."https://github.com".helper = "!${pkgs.github-cli}/bin/gh auth git-credential";
+            credential."https://gist.github.com".helper = "!${pkgs.github-cli}/bin/gh auth git-credential";
         };
+    };
 
     programs.direnv = {
         enable = true;
@@ -72,76 +87,76 @@ in {
     programs.home-manager.enable = true;
 
     home.file.".config/git/attributes".text = ''
-    *.java merge=mergiraf
-    *.properties merge=mergiraf
-    *.kt merge=mergiraf
-    *.rs merge=mergiraf
-    *.go merge=mergiraf
-    go.mod merge=mergiraf
-    go.sum merge=mergiraf
-    *.ini merge=mergiraf
-    *.js merge=mergiraf
-    *.jsx merge=mergiraf
-    *.mjs merge=mergiraf
-    *.json merge=mergiraf
-    *.yml merge=mergiraf
-    *.yaml merge=mergiraf
-    pyproject.toml merge=mergiraf
-    *.toml merge=mergiraf
-    *.html merge=mergiraf
-    *.htm merge=mergiraf
-    *.xhtml merge=mergiraf
-    *.xml merge=mergiraf
-    *.c merge=mergiraf
-    *.h merge=mergiraf
-    *.cc merge=mergiraf
-    *.hh merge=mergiraf
-    *.cpp merge=mergiraf
-    *.hpp merge=mergiraf
-    *.cxx merge=mergiraf
-    *.hxx merge=mergiraf
-    *.c++ merge=mergiraf
-    *.h++ merge=mergiraf
-    *.mpp merge=mergiraf
-    *.cppm merge=mergiraf
-    *.ixx merge=mergiraf
-    *.tcc merge=mergiraf
-    *.cs merge=mergiraf
-    *.dart merge=mergiraf
-    *.dts merge=mergiraf
-    *.scala merge=mergiraf
-    *.sbt merge=mergiraf
-    *.ts merge=mergiraf
-    *.tsx merge=mergiraf
-    *.py merge=mergiraf
-    *.php merge=mergiraf
-    *.phtml merge=mergiraf
-    *.sol merge=mergiraf
-    *.lua merge=mergiraf
-    *.rb merge=mergiraf
-    *.ex merge=mergiraf
-    *.exs merge=mergiraf
-    *.nix merge=mergiraf
-    *.sv merge=mergiraf
-    *.svh merge=mergiraf
-    *.md merge=mergiraf
-    *.hcl merge=mergiraf
-    *.tf merge=mergiraf
-    *.tfvars merge=mergiraf
-    *.ml merge=mergiraf
-    *.mli merge=mergiraf
-    *.hs merge=mergiraf
-    *.mk merge=mergiraf
-    Makefile merge=mergiraf
-    GNUmakefile merge=mergiraf
-    *.bzl merge=mergiraf
-    *.bazel merge=mergiraf
-    BUILD merge=mergiraf
-    WORKSPACE merge=mergiraf
-    *.cmake merge=mergiraf
-    *.swift merge=mergiraf
-    CMakeLists.txt merge=mergiraf
-    '';
+        *.java merge=mergiraf
+        *.properties merge=mergiraf
+        *.kt merge=mergiraf
+        *.rs merge=mergiraf
+        *.go merge=mergiraf
+        go.mod merge=mergiraf
+        go.sum merge=mergiraf
+        *.ini merge=mergiraf
+        *.js merge=mergiraf
+        *.jsx merge=mergiraf
+        *.mjs merge=mergiraf
+        *.json merge=mergiraf
+        *.yml merge=mergiraf
+        *.yaml merge=mergiraf
+        pyproject.toml merge=mergiraf
+        *.toml merge=mergiraf
+        *.html merge=mergiraf
+        *.htm merge=mergiraf
+        *.xhtml merge=mergiraf
+        *.xml merge=mergiraf
+        *.c merge=mergiraf
+        *.h merge=mergiraf
+        *.cc merge=mergiraf
+        *.hh merge=mergiraf
+        *.cpp merge=mergiraf
+        *.hpp merge=mergiraf
+        *.cxx merge=mergiraf
+        *.hxx merge=mergiraf
+        *.c++ merge=mergiraf
+        *.h++ merge=mergiraf
+        *.mpp merge=mergiraf
+        *.cppm merge=mergiraf
+        *.ixx merge=mergiraf
+        *.tcc merge=mergiraf
+        *.cs merge=mergiraf
+        *.dart merge=mergiraf
+        *.dts merge=mergiraf
+        *.scala merge=mergiraf
+        *.sbt merge=mergiraf
+        *.ts merge=mergiraf
+        *.tsx merge=mergiraf
+        *.py merge=mergiraf
+        *.php merge=mergiraf
+        *.phtml merge=mergiraf
+        *.sol merge=mergiraf
+        *.lua merge=mergiraf
+        *.rb merge=mergiraf
+        *.ex merge=mergiraf
+        *.exs merge=mergiraf
+        *.nix merge=mergiraf
+        *.sv merge=mergiraf
+        *.svh merge=mergiraf
+        *.md merge=mergiraf
+        *.hcl merge=mergiraf
+        *.tf merge=mergiraf
+        *.tfvars merge=mergiraf
+        *.ml merge=mergiraf
+        *.mli merge=mergiraf
+        *.hs merge=mergiraf
+        *.mk merge=mergiraf
+        Makefile merge=mergiraf
+        GNUmakefile merge=mergiraf
+        *.bzl merge=mergiraf
+        *.bazel merge=mergiraf
+        BUILD merge=mergiraf
+        WORKSPACE merge=mergiraf
+        *.cmake merge=mergiraf
+        *.swift merge=mergiraf
+        CMakeLists.txt merge=mergiraf
+        '';
 
     home.sessionVariables = {
         EDITOR = "nvim";

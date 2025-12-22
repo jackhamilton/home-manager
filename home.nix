62057eb1fa539a3ce50bfd8030b353540a978c8b
jackhamilton <jackham800@gmail.com>
2025-12-21 20:01:10 -0500

    update

--- home.nix
+++ home.nix
@@ -16,6 +16,19 @@ in {
             name = "Catppuccin-Mocha";
             package = pkgs.catppuccin-gtk;
         };
+
+        gtk3.extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+        };
+        gtk4.extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+        };
+    };
+
+    xdg.configFile = {
+      "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+      "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+      "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
     };
 
     qt = {
@@ -30,6 +43,9 @@ in {
 
     home.packages = with pkgs;
     (with pkgs; [
+    numix-icon-theme-circle
+    catppuccin-qt5ct
+    catppuccin-gtk
      lsd
      bat
      curlie
--- nixos.nix
+++ nixos.nix
@@ -1,84 +1,90 @@
 { config, pkgs, pkgs-unstable, lib, ... }:
 {
+    "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+    };
     services.udiskie = {
         enable = true;
+        automount = true;
+        notify = true;
         settings = {
-            # workaround for
-            # https://github.com/nix-community/home-manager/issues/632
+# workaround for
+# https://github.com/nix-community/home-manager/issues/632
             program_options = {
-                # replace with your favorite file manager
+# replace with your favorite file manager
                 file_manager = "${pkgs.nautilus}/bin/nautilus";
             };
         };
     };
 
     home.packages = (with pkgs; [
-        gradience
-        nautilus
-        solarc-gtk-theme
-        autotiling
-        bazelisk
-        bitwarden-menu
-        bitwarden-cli
-        chezmoi
-        cifs-utils
-        samba4Full
-        clipman
-        dconf-editor
-        dhcpcd
-        dmenu
-        dpkg
-        dunst
-        evince
-        file-roller
-        flameshot
-        fuzzel
-        gamescope
-        gammastep
-        grim
-        inetutils
-        lutris
-        lxappearance
-        mako
-        mangohud
-        mimeo
-        neomutt
-        networkmanager
-        networkmanagerapplet
-        nitrogen
-        nushell
-        nwg-bar
-        nwg-displays
-        nwg-wrapper
-        orca
-        pamixer
-        papirus-icon-theme
-        paru
-        pavucontrol
-        pipewire
-        wireplumber
-        protonvpn-gui
-        protonvpn-cli
-        qmk
-        qemu
-        rofi
-        ruby
-        slurp
-        waybar
-        wlsunset
-        wofi
-        xdg-desktop-portal
-        xdg-desktop-portal-wlr
-        hyprpolkitagent
-        kdePackages.xwaylandvideobridge
-        xdg-terminal-exec
-        hyprsunset
-        firefox
-    ])
-    ++ (with pkgs-unstable; [
-        mergiraf
-        # hyprland
-        # hyprsunset
-        # xdg-desktop-portal-hyprland
-    ]);
+            gradience
+            nautilus
+            solarc-gtk-theme
+            autotiling
+            bazelisk
+            bitwarden-menu
+            bitwarden-cli
+            chezmoi
+            cifs-utils
+            samba4Full
+            clipman
+            dconf-editor
+            dhcpcd
+            dmenu
+            dpkg
+            dunst
+            evince
+            file-roller
+            flameshot
+            fuzzel
+            gamescope
+            gammastep
+            grim
+            inetutils
+            lutris
+            lxappearance
+            mako
+            mangohud
+            mimeo
+            neomutt
+            networkmanager
+            networkmanagerapplet
+            nitrogen
+            nushell
+            nwg-bar
+            nwg-displays
+            nwg-wrapper
+            orca
+            pamixer
+            papirus-icon-theme
+            paru
+            pavucontrol
+            pipewire
+            wireplumber
+            protonvpn-gui
+            protonvpn-cli
+            qmk
+            qemu
+            rofi
+            ruby
+            slurp
+            waybar
+            wlsunset
+            wofi
+            xdg-desktop-portal
+            xdg-desktop-portal-wlr
+            hyprpolkitagent
+            kdePackages.xwaylandvideobridge
+            xdg-terminal-exec
+            hyprsunset
+            firefox
+            udiskie
+            ])
+            ++ (with pkgs-unstable; [
+                    mergiraf
+# hyprland
+# hyprsunset
+# xdg-desktop-portal-hyprland
+            ]);
 }

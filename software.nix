3cc2d4fe9c99e9f5984ac03481c4ad1ae200c21f
jackhamilton <jackham800@gmail.com>
2026-01-09 11:04:03 -0600

    Push

--- software.nix
+++ software.nix
@@ -31,7 +31,22 @@
     ])
     ++ (with pkgs-unstable; [
       godot
-    ]);
+    ])
+    ++ [
+      (pkgs.symlinkJoin {
+        name = "zen-browser";
+        paths = [ inputs.zen-browser.packages.${pkgs.system}.twilight ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/zen \
+            --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [
+              pkgs.ffmpeg-full
+              pkgs.mediastreamer
+              pkgs.openh264
+            ]}"
+        '';
+      })
+    ];
 
   programs.anki = {
     enable = true;
